7be9cb6269b61c4b4f280b8ebbac30b7281dfae6
Lautaro <Lautaro@DESKTOP-70FPLRL>
2024-10-03 18:44:05 -0300

    Agregar archivos de proyecto.

---
+++ AppStore/mvvm/ViewModels/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿using AppStore.mvvm.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Controls.Shapes;
+
+namespace AppStore.mvvm.ViewModels;
+
+public partial class LoginViewModel : BaseViewModel
+{
+    public LoginViewModel()
+    {
+        Title = "Login";
+    }
+
+    [ObservableProperty] private string username = string.Empty;
+    [ObservableProperty] private string contrasena = string.Empty;
+    [ObservableProperty] private string message = string.Empty;
+
+    [RelayCommand]
+    public async Task LoginAsync2()
+    {
+        // await Application.Current.MainPage.DisplayAlert("Login", "Login", "Ok");
+
+        await Application.Current.MainPage.Navigation.PushAsync(new ProductoListaPage(new ProductoListaViewModel()));
+    }
+
+    [RelayCommand]
+    public async Task LoginAsync()
+    {
+        if (IsBusy)
+        {
+            IsBusy = true;
+
+            // asignamos objeto con datos del usuario-establecimiento logueados
+            if (Username != string.Empty && Contrasena!= String.Empty)
+            {
+                var apiClient = new ApiService();
+
+                var login = await apiClient.ValidarLogin(Username, Contrasena);
+
+                if (login != null)
+                {                    
+                    Application.Current.MainPage = new NavigationPage(new ProductoListaPage(new ProductoListaViewModel()));
+                   
+                   
+                    Transport.IdUsuario = 1;
+                            
+                    
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Atención", "Las credenciales ingresadas no son válidas", "Aceptar");
+                }
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Atención", "Las credenciales son Obligatorias. Verifique!", "Aceptar");
+            }
+
+            IsBusy = true;
+        }
+
+    }
+}
